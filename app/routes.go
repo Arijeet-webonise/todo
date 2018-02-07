afa706c0f5fbc6190e7899d7b7362127666fdde0
Arijeet Baruah <arijeet@oysterlabs.com>
2018-02-07 18:07:45 +0530

    Static File

--- app/routes.go
+++ app/routes.go
@@ -1,5 +1,7 @@
 package app
 
+import "net/http"
+
 // InitRouter will intialise the router
 func (app *App) InitRouter() {
 	initialiseV1API(app)
@@ -14,5 +16,9 @@ func initialiseV1API(app *App) {
 	//VIEW
 	app.Router.Get("/", app.renderView(app.RenderIndex))
 	app.Router.Get("/todo/", app.renderView(app.DisplayTodos))
+
+	//STATIC
+	fs := http.FileServer(http.Dir("assets/"))
+	app.Router.Post("/static/", http.StripPrefix("/static/", fs))
 
 }
